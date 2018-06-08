98907b844dbefa4d66115805942dde214225f3fa
vampiresska <lasovayuliia@gmail.com>
2018-06-08 12:29:13 +0300

    BubbleSort optimization

--- lesson7/src/arr/Arr.java
+++ lesson7/src/arr/Arr.java
@@ -24,14 +24,18 @@ public class Arr {
     }
 
     public static int[] arrBubbleSortDesc(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 1; j < arr.length; j++) {
+        boolean isSorted;
+        for (int i = 0; i < arr.length - 2; i++) {
+            isSorted = true;
+            for (int j = 1; j < arr.length-i; j++) {
                 if (arr[j] > arr[j - 1]) {
                     int buff = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = buff;
+                    isSorted = false;
                 }
             }
+            if (isSorted) break;
         }
         return arr;
     }
